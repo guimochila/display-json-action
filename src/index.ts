fd8ce6050e613d72fa648fca373c1a6e9dd6c51b
Guilherme <gui.mochila@gmail.com>
2023-01-11 15:38:02 +0100

    Testing workflows

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import * as core from '@actions/core'
 import path from 'path'
 import Handlebars from 'handlebars'
+import github from '@actions/github'
 
 function htmlTemplate(): string {
   return `
@@ -27,7 +28,14 @@ function readJSONFile(filePath: string): string | undefined {
 
 async function run(): Promise<void> {
   try {
-    const file = core.getInput('file')
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const pr_number = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
+    const file = core.getInput('file', { required: true })
+
+    const octokit = github.getOctokit(token)
+
     const fileContent = readJSONFile(file)
     const template = Handlebars.compile(htmlTemplate())
 
@@ -35,7 +43,12 @@ async function run(): Promise<void> {
       throw new Error('Error reading file')
     }
 
-    core.setOutput('content', template(JSON.parse(fileContent)))
+    octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: parseInt(pr_number),
+      body: template(JSON.parse(fileContent)),
+    })
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
